904720a732302616dff82737972d920c70f5b80d
Daniel Carvalho <danielscarvalho@gmail.com>
2022-02-27 19:37:56 -0300

    New code...

--- main.py
+++ main.py
@@ -1,13 +1,51 @@
 import os
-
 from flask import Flask
+from flask import request
+from datetime import datetime
+import insperds
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    name = os.environ.get("NAME", "World")
-    return "Hello {}!, from Insper DS!!".format(name)
+@app.route('/')
+def web_root():
+    now = datetime.now()
+    return 'Hey, we have Flask in a Docker container by Insper DS! - ' + str(now) 
+
+@app.route('/core')
+def core():
+    now = datetime.now()
+    return 'Vai Corinthians!! - ' + str(now) 
+
+@app.route('/add/<a>/<b>')
+def add(a, b):
+    return str(float(a) + float(b))
+
+@app.route('/area')
+def myarea():
+  altura = request.args.get('altura', default = 0, type = float)
+  largura = request.args.get('largura', default = 0, type = float)
+  comprimento = request.args.get('comprimento', default = -1, type = float)
+  
+  if (comprimento < 0):
+        return str(altura*largura)
+  else:
+        return str(altura*largura*comprimento)
+
+@app.route('/query/<text>')
+def query(text):
+    return insperds.ddgquery(text)
+    
+@app.route('/bitcoins')
+def btc():
+    return insperds.bitcoins()
+    
+@app.route('/ethereum')
+def ether():
+    return insperds.ethereum()
+    
+@app.route('/weather/<lat>/<lon>')
+def weather(lat, lon):
+    return insperds.weather(lat, lon)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
